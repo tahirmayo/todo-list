dd424a3267d90ac9cf6582435c974bbd1cbdaff1
tahirmayo <68633605+tahirmayo@users.noreply.github.com>
2020-09-09 17:15:40 +0500

    Updates

---
+++ precache-manifest.232596fab9249f589ce1933b5aed13b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccae9b5cfda7c9120efce15b7645992",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3622d9b68b973aba51ec",
+    "url": "/todo-list/static/css/main.60784a85.chunk.css"
+  },
+  {
+    "revision": "8949b2bb52a7336db61c",
+    "url": "/todo-list/static/js/2.4c153ab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.4c153ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3622d9b68b973aba51ec",
+    "url": "/todo-list/static/js/main.3b476a87.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
